5c8d5b8fcb706cd95d13443b3d3a4fd637a3b429
richmondafari <afaririchmond1000@gmail.com>
2025-07-21 23:46:07 +0000

    integrate the AI model, install libraries and define logic for disease prdiction

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,6 @@
 from  rest_framework import serializers
 
+
+# Define the serializser to handle file uplaods from the frontend
 class PredictionSerializer(serializers.Serializer):
     image = serializers.ImageField()
